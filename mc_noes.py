6b66e0012e7f8e554b416d7c5d7f6c891ea215b3
ragonc <carmine.f.ragone@gmail.com>
2018-12-30 20:51:49 +0100

    noes_with comments

--- mc_noes.py
+++ mc_noes.py
@@ -5,54 +5,60 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     env = gym.make('Settemezzo-v0')
-    EPS = 0.1
+    EPS = 0.05
     GAMMA = 0.9
 
+#Setting up the Q-list that we are going to be working with
     Q = {}
     agentSumSpace = [i for i in range(1, 30)]
     dealerShowCardSpace = [i + 1 for i in range(15)]
     actionSpace = [0, 1]  # stick or hit
-
+    
+#Creation and initialisation of the variables
     stateSpace = []
     returns = {}
     pairsVisited = {}
+    
+#I teration over the state space and creation of tuples out of those
     for total in agentSumSpace:
         for card in dealerShowCardSpace:
             for action in actionSpace:
                 Q[((total, card), action)] = 0
                 returns[((total, card), action)] = 0
-                pairsVisited[((total, card), action)] = 0
-            stateSpace.append((total, card))
+                pairsVisited[((total, card), action)] = 0 #keep track of how many times you visited a point
+            stateSpace.append((total, card)) #create a list out of it and append the results
 
-    policy = {}
+    policy = {} #tells the agent what to do in any given space
     for state in stateSpace:
-        policy[state] = np.random.choice(actionSpace)
+        policy[state] = np.random.choice(actionSpace) #start with stick or hit with 50% probability (random policy)
 
     numEpisodes = 1000000
     for i in range(numEpisodes):
-        statesActionsReturns = []
-        memory = []
-        if i % 100000 == 0:
-            print('starting episode', i)
+        statesActionsReturns = [] #empty list to keep track of state actions
+        memory = [] #memory to append the episodes
+        if i % 100000 == 0: #to keep track of the training (not mandatory, but useful)
+            print('Starting Episode', i)
         observation = env.reset()
-        done = False
+        done = False #reset till you are not done 
+
+#To actually play the game you need the following cycle
         while not done:
-            action = policy[observation]
+            action = policy[observation] #select an action, given the observation of the environment
             observation_, reward, done, info = env.step(action)
             memory.append((observation[0], observation[1], action, reward))
-            observation = observation_
-        memory.append((observation[0], observation[1], action, reward))
+            observation = observation_ #reset to the new state
+        memory.append((observation[0], observation[1], action, reward)) #when the episode is over append the final state
 
-        G = 0
+        G = 0 
         last = True
         for playerSum, dealerCard, action, reward in reversed(memory):
             if last:
                 last = False
             else:
-                statesActionsReturns.append((playerSum, dealerCard, action, G))
+                statesActionsReturns.append((playerSum, dealerCard, action, G)) #return after the first visit
             G = GAMMA * G + reward
 
-        statesActionsReturns.reverse()
+        statesActionsReturns.reverse() #reverse to put in chronological order
         statesActionsVisited = []
 
         for playerSum, dealerCard, action, G in statesActionsReturns:
